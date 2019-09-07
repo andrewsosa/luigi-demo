ce87f7fc503dc1d994d89340f9ec95656d03002c
Andrew Sosa <andrew@andrewsosa.com>
2019-09-07 15:38:29 -0400

    add updated files

--- pipeline/extract/posts.py
+++ pipeline/extract/posts.py
@@ -1,1 +1,17 @@
+import luigi
+from luigi import contrib
 from pymongo import MongoClient
+from .. import config
+
+
+class ExtractMongoPosts(luigi.Task):
+    posts_from = luigi.DateParameter()
+    client = MongoClient(config.MONGO_URI)
+
+    def output(self):
+        return luigi.LocalTarget("data/posts%s.tsv" % self.date_interval)
+
+    def run(self):
+        db = self.client['snews']
+        reddit_posts = db['reddit-posts']
+
